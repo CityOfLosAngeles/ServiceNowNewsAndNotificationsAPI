ff9e94fc8c5363e178d2071d19978863da6f90a2
22758 <22758@982b268a-0adc-5e43-aa82-c372db4beac7>
2016-08-30 23:00:29 +0000

    init
    git-svn-id: http://10.194.35.252/svn/ServiceNow/trunk/ServiceNowNewsAndNotificationsAPI@3 982b268a-0adc-5e43-aa82-c372db4beac7

---
+++ ServiceNowNewsAndNotificationsAPI/Controllers/OutageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Web.Http;
+using ServiceNowNewsAndNotificationsAPI.Models;
+using System.Web.Script.Serialization;
+
+
+namespace ServiceNowNewsAndNotificationsAPI.Controllers
+{
+    public class OutageController : ApiController
+    {
+        [HttpGet]
+        [ActionName("getProblems")]
+        public HttpResponseMessage getProblems()
+        {
+            try
+            {
+                //Incident URL
+                string incidentURL = "https://cityoflaprod.service-now.com/api/now/table/incident?active=true&u_outage=true";
+                
+                //Problem URL
+                string problemURL = "https://cityoflaprod.service-now.com/api/now/table/problem?active=true&u_outage=true";
+
+                // Incident Connection
+                ServiceNowRequest webRequest = new ServiceNowRequest(incidentURL, "GET");
+                var incidentData = webRequest.GetResponse();
+
+                webRequest = new ServiceNowRequest(problemURL, "GET");
+                var problemData = webRequest.GetResponse();
+
+                var getData = new ProcessData();
+                var list = getData.DataMassage(incidentData, problemData);
+
+                var json = new JavaScriptSerializer().Serialize(list);
+
+                return Request.CreateResponse(HttpStatusCode.OK, json);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
---
+++ ServiceNowNewsAndNotificationsAPI/Models/Problem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceNowNewsAndNotificationsAPI.Models
+{
+    public class Problem
+    {
+        public Problem()
+        {
+            Incidents = new List<Incident>();
+        }
+        public string ProblemNum { get; set; }
+        public string SysId { get; set; }
+        public string OutageStartDateTime { get; set; }
+        public string OutageEndDateTime { get; set; }
+        public string OutageType { get; set; }
+        public string OutageScope { get; set; }
+        public string OutageStatus { get; set; }
+        public string ShortDescription { get; set; }
+        public string CreatedDt { get; set; }
+        public string ProblemLink { get; set; }
+
+        public List<Incident> Incidents { get; set; }
+
+    }
+}
