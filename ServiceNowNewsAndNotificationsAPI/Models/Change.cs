29f55de02f7f23ceb021bf59f85c3b7bd0370ef4
22758 <22758@982b268a-0adc-5e43-aa82-c372db4beac7>
2016-09-22 15:53:19 +0000

    1. Added code to change UTC time to local time. 2. Crated new class for Planned Outages. 3. Added new method to OutageController.cs to return planned outage data.
    git-svn-id: http://10.194.35.252/svn/ServiceNow/trunk/ServiceNowNewsAndNotificationsAPI@23 982b268a-0adc-5e43-aa82-c372db4beac7

---
+++ ServiceNowNewsAndNotificationsAPI/Models/Change.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceNowNewsAndNotificationsAPI.Models
+{
+    public class Change
+    {
+        public string ChangeNumber { get; set; }
+        public string ChangeShortDescription { get; set; }
+        public string ChangeDescription { get; set; }
+        public string PlannedChangeStartDate { get; set; }
+        public string PlannedChangeEndDate { get; set; }
+        public string ChangeApprovalStatus { get; set; }
+        public string ChangeState { get; set; }
+        public string ChangeRisk { get; set; }
+        public string ChangePriority { get; set; }
+        public string ChangeImpact { get; set; }
+        public string ChangeLink { get; set; }
+    }
+}
