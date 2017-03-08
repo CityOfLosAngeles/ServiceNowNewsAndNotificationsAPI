e28a53e9e0006d1a347c0bece9f8acb50117b41e
333075 <333075@982b268a-0adc-5e43-aa82-c372db4beac7>
2017-03-08 23:38:19 +0000

    1. Removed unused fields from Incident and Problem 2. Fixed ServiceNow URL Feed 3. Added filters to exluded LAPD events
    git-svn-id: http://10.194.35.252/svn/ServiceNow/trunk/ServiceNowNewsAndNotificationsAPI@28 982b268a-0adc-5e43-aa82-c372db4beac7

--- ServiceNowNewsAndNotificationsAPI/Models/Incident.cs
+++ ServiceNowNewsAndNotificationsAPI/Models/Incident.cs
@@ -8,17 +8,17 @@ namespace ServiceNowNewsAndNotificationsAPI.Models
     public class Incident
     {
         public string IncidentNum { get; set; }
-        public string OutageStartDateTime { get; set; }
-        public string OutageEndDateTime { get; set; }
-        public string OutageType { get; set; }
+        //public string OutageStartDateTime { get; set; }
+        //public string OutageEndDateTime { get; set; }
+        //public string OutageType { get; set; }
         public string OutageScope { get; set; }
         public string ShortDescription { get; set; }
         public string IncidentStatus { get; set; }
         public string CreatedDt { get; set; }
         public string ProblemId { get; set; }
         public string ClosedDt { get; set; }
-        public string OutageStartDt { get; set;}
-        public string OutageStartTm { get; set; }
+        //public string OutageStartDt { get; set; }
+        //public string OutageStartTm { get; set; }
         public string ProblemLink { get; set; }
         public string IncidentLink { get; set; }
         public string ProblemNum { get; set; }
--- ServiceNowNewsAndNotificationsAPI/Models/Knowledge.cs
+++ ServiceNowNewsAndNotificationsAPI/Models/Knowledge.cs
@@ -16,5 +16,21 @@ namespace ServiceNowNewsAndNotificationsAPI.Models
         public string WorkflowStatus { get; set; }
         public string PublishedDt { get; set; }
         public string ValidToDt { get; set; }
+
+        public string KBNewsCategory
+        {
+            get
+            {
+                return "f58f35a7dbd9a200e88577e9af96198e"; //News
+            }
+        }
+
+        public string KBNewsOutagesKnowledgeBase
+        {
+            get
+            {
+                return "ba4b2059db98a2003fc6f1fcbf961987"; //Knews and Outages
+            }
+        }
     }
 }
